9fa57ea18240715d7376708fdc3863ae7c0640ca
Yogesh Chauhan <145284275+yogesh000001@users.noreply.github.com>
2024-09-03 01:08:09 +0530

    Time: 3 ms (48.64%), Space: 50 MB (6.61%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -3,7 +3,7 @@ class Solution {
         int max = 0;
         int ans = 0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==1){
+            if(nums[i] == 1){
                 max++;
                 ans = Math.max(ans,max);
             }else{
